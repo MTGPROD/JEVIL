ac8a52529b5a54fdff79338f01664007ba95373d
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-01-08 00:11:01 +0100

    Update server.js

--- .gitignore/server.js
+++ .gitignore/server.js
@@ -6,4 +6,28 @@ client.on('ready', () => {
     client.user.setStatus('dnd')
 })
 
+
+
+client.on('message', message => {
+    let args = message.content.slice(prefix.length).trim().split(' ');
+    let cmd = args.shift().toLowerCase();
+
+    if(message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+
+    try {
+        delete require.cache[require.resolve(`./cmds/${cmd}.js`)]
+
+        
+      
+            
+
+
+        let commandFile = require(`./commands/${cmd}.js`)
+        commandFile.run(client, message, args, ops);
+    } catch (e) {
+        console.log(e.stack)
+    }
+});
+
 client.login(process.env.TOKEN)
