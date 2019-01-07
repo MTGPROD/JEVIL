a87eb5521917c12e308d3c1e06b3513f17eba678
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-01-08 00:03:19 +0100

    Update server.js

--- .gitignore/server.js
+++ .gitignore/server.js
@@ -4,6 +4,6 @@ const client = new Discord.Client()
 client.on('ready', () => {
     client.user.setGame('I CAN DO ANYTHING')
     client.user.setStatus('dnd')
-}
+})
 
 client.login(process.evn.TOKEN)
