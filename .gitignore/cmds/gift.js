57effb1bff9483437f90d90d1fdb8fff2c07496a
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-01-10 23:13:13 +0100

    Create gift.js

---
+++ .gitignore/cmds/gift.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js')
+
+exports.run = (client, message, args) => {
+     var c1 = '🤣'
+     var c2 = '🙂'
+     var c3 = '🤔'
+     var c4 = '😀'
+     var c5 = '😅'
+     
+     const embed = new Discord.RichEmbed()
+           .setTitle('Gift')
+           .setDescription(`[===============]\n${c1} | ${c2} | ${c3} | ${c4} | ${c5}\n[=======^======]`)
+     message.channel.send(embed)
+})
