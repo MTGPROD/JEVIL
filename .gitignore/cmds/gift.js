6fd6a460853994457af307bf18298cc21b0fe667
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-01-11 17:09:41 +0100

    Update gift.js

--- .gitignore/cmds/gift.js
+++ .gitignore/cmds/gift.js
@@ -1,17 +1,21 @@
 const Discord = require('discord.js')
 
 exports.run = (client, message, args) => {
-     var c1 = '🤣'
-     var c2 = '🙂'
-     var c3 = '🤔'
-     var c4 = '😀'
-     var c5 = '😅'
+     var randome = ['🤣', '😋', '😅', '😙', '😎']
+     var gift = random[Math.floor(Math.random * randome.length)]
+     
+     
+
      
      const embed = new Discord.RichEmbed()
            .setTitle('Gift')
-           .setDescription(`[===============]\n${c1} | ${c2} | ${c3} | ${c4} | ${c5}\n[======^========]`)
+           .setDescription(`${gift}`)
      message.channel.send(embed).then(msg => {
-          embed.setDescription(`[===============]\n${c5} | ${c1} | ${c2} | ${c3} | ${c4}\n[======^========]`)
-          msg.edit(embed)
+          setInterval(function() => {
+               embed.setDescription(`${gift}`)
+               msg.edit(embed)
+          }, 900)
+          
+          
      })
 }
