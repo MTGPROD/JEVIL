75a1ed6a164506bd0a61d8a0e0b47a4c6c1c5a83
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-03-20 21:35:03 +0000

    Add files via upload

--- .gitignore/cmds/smash.js
+++ .gitignore/cmds/smash.js
@@ -1,7 +1,29 @@
 const Discord = require('discord.js')
 
 exports.run = (client, args, message, gameData, p1, p2, p3, p4) => {
-	if(!message.member.roles.has('553308822169583653')) return message.channel.send("Tu n'as pas le rôle <@553308822169583653>")
+	if(message.author.id === p1.id) {
+		if(p1.mj === false) return message.channel.send("Tu n'est pas MJ.")
+    } else {
+    	return message.channel.send("Tu n'est pas dans la partie.")
+    }
+    
+    if(message.author.id === p2.id) {
+		if(p2.mj === false) return message.channel.send("Tu n'est pas MJ.")
+    } else {
+    	return message.channel.send("Tu n'est pas dans la partie.")
+    }
+    
+    if(message.author.id === p3.id) {
+		if(p3.mj === false) return message.channel.send("Tu n'est pas MJ.")
+    } else {
+    	return message.channel.send("Tu n'est pas dans la partie.")
+    }
+    
+    if(message.author.id === p4.id) {
+		if(p4.mj === false) return message.channel.send("Tu n'est pas MJ.")
+    } else {
+    	return message.channel.send("Tu n'est pas dans la partie.")
+    }
 	gameData.smashBall = true
 	message.channel.send('Le premier qui dit `SMASH` pourra utiliser son pourvoir Smash.')
 	const filter = m => m.content.includes('SMASH') && message.author.id != '497101514079469569'
