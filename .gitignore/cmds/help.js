3061e41ceae9cf8602fc7e6515e78ae8cc2ae9bc
MTGPROD <40500781+MTGPROD@users.noreply.github.com>
2019-03-25 17:49:37 +0100

    Update help.js

--- .gitignore/cmds/help.js
+++ .gitignore/cmds/help.js
@@ -3,6 +3,9 @@ const Discord = require('discord.js')
 exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
         .setTitle('SSE - Help')
-        .setDescription('Arrivera plus tard.')
+        .setDescription("Voici le menu d'aide pour le jeu SSE")
+        .addField('Aide pour - Partie en cours', "`+game create`: Créé une partie, si ce n'est pas déjà fait.\n`+game join`: Rejoindre une partie en cours.\n`+game reset`: Arrête la partie et remet a zéro la database.")
+        
+                      
     message.channel.send(embed)
 }
